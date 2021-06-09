2f791cbc4f08d074cca17d539626b75ebb4cbcd6
choyo <choyo@DESKTOP-UDM56M3>
2021-06-09 21:47:00 +0900

    java week 12 이론 (2)

--- hello_spring/src/main/java/kr/ac/inha/mvc/controller/JspController.java
+++ hello_spring/src/main/java/kr/ac/inha/mvc/controller/JspController.java
@@ -8,10 +8,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class JspController {
 	@RequestMapping("/hello.do")
-	public ModelAndView hello () {
+	public ModelAndView hellodo () {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("hello");
+		mv.setViewName("hello"); // 파일 헬로와 연동하여 보여줌
 		
 		return mv;
+	}
+	@RequestMapping("/hello.dodo")
+	public String hellododo () {
+		
+		return "hello"; // 파일 헬로와 자동으로 modelandview 객체를 생성하여 맵핑
 	}
 }
---
+++ hello_spring/src/main/java/kr/ac/inha/mvc/service/JspService.java
@@ -0,0 +1,8 @@
+package kr.ac.inha.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JspService {
+	//Dao 에 해당하는 어노테이션을 사용
+}
